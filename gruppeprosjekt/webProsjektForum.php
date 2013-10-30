b8ac7ef8c437e91f288993b0408ffd8bd9c9b90b
kasper70 <kasper_jaco@dhcp-158-39-199-137.wlan.hioa.no>
2013-10-30 13:33:45 +0100

    oppdatert

---
+++ gruppeprosjekt/webProsjektForum.php
@@ -0,0 +1,1 @@
+<aside>hei</aside>
---
+++ gruppeprosjekt/webProsjektTopp.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<meta charset="UTF-8">
+<title>fitness</title>
+<link rel="stylesheet" type="text/css" href="webProsjektStilark.css">
+</head>
+<body>
+
+
+<!--topp-->
+<header>
+<div id="slide"><img src="styrke1.png" name="slide" alt="bilder"></div>
+</header>
+<!--Navigasjon-->
+
+<ul id="navigasjonsbar">
+<li><a href="webProsjektHoved.php">Hjem</a></li>
+<li><a href="webProsjektHoved.php?page=1">Trening</a></li>
+<li><a href="webProsjektHoved.php?page=2">Kosthold</a></li>
+<li><a href="webProsjektHoved.php?page=3">Butikk</a></li>
+<li><a href="webProsjektHoved.php?page=4">Forum</a></li>
+<li><a href="webProsjektHoved.php?page=5">Kontakt</a></li>
+</ul>
+</nav>
+
+<!--Slider script-->
+<?php
+include ("slider.js");
+?>
+
