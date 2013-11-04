04f7b3fa4a3aed431dad828d6033c43175f4c557
matslo <mats_loekken@hotmail.com>
2013-11-04 11:27:59 +0100

    endret

--- GruppeWEB/webProsjektTreningStyrke.php
+++ GruppeWEB/webProsjektTreningStyrke.php
@@ -25,76 +25,51 @@ $(document).ready(function linker(){
 
 <table border="1">
 <tr>
-<th>Mandag</th>
-<th>Tirsdag</th>
-<th>Onsdag</th>
-<th>Torsdag</th>
-<th>Fredag</th>
-<th>Lørdag</th>
-<th>Søndag</th>
+<th>Dag 1</th>
+<th>Dag 2</th>
+<th>Dag 3</th>
+<th>Dag 4</th>
 </tr>
 <tr>
-<td class="mandag">f</td>
-<td class="tirsdag"></td>
-<td class="onsdag"></td>
-<td class="torsdag"></td>
-<td class="fredag"></td>
-<td class="lørdag"></td>
-<td class="søndag"></td>
+<td class="dag1">Benkpress.4*4-6 reps</td>
+<td class="dag2">Knebøy 4*4-6 reps</td>
+<td class="dag3">Benkpress droppsett type 1</td>
+<td class="dag4">Markløft droppsett type 1</td>
 </tr>
 <tr>
-<td class="mandag">f</td>
-<td class="tirsdag"></td>
-<td class="onsdag"></td>
-<td class="torsdag"></td>
-<td class="fredag"></td>
-<td class="lørdag"></td>
-<td class="søndag"></td>
+<td class="dag1">Kabelroing 4*6-8 reps</td>
+<td class="dag2">Strak markløft 3*6-8 reps</td>
+<td class="dag3">Skrå manualbenk 3*8-10 reps</td>
+<td class="dag4">Chins 3*8-10 reps</td>
 </tr>
 <tr>
-<td class="mandag">f</td>
-<td class="tirsdag"></td>
-<td class="onsdag"></td>
-<td class="torsdag"></td>
-<td class="fredag"></td>
-<td class="lørdag"></td>
-<td class="søndag"></td>
+<td class="dag1">Skuldepress med stang 3*4-6 reps</td>
+<td class="dag2">Bulgarsk utfall 3*6-8 reps</td> 
+<td class="dag3">Flies variant 3*12-15 reps</td>
+<td class="dag4">Benkroing 4*10-12 reps</td>
+<tr>
+<td class="dag1">Chins 4*4-6 reps</td>
+<td class="dag2">Hiptrust 3*6-8 reps</td>
+<td class="dag3">Sidehev variant 4*10-12 reps</td>
+<td class="dag4">Rear delt flies 3*12-15 reps</td>
 </tr>
 <tr>
-<td class="mandag"></td>
-<td class="tirsdag"></td>
-<td class="onsdag"></td>
-<td class="torsdag"></td>
-<td class="fredag"></td>
-<td class="lørdag"></td>
-<td class="søndag"></td>
+<td class="dag1">Dips 3*4-6 reps</td>
+<td class="dag2">Hengende beinhev 3×8 reps</td>
+<td class="dag3">Frankspress 3*8-10 reps</td>
+<td class="dag4">Bicepscurl 3*8-10 reps</td>
 </tr>
 <tr>
-<td class="mandag"></td>
-<td class="tirsdag"></td>
-<td class="onsdag"></td>
-<td class="torsdag"></td>
-<td class="fredag"></td>
-<td class="lørdag"></td>
-<td class="søndag"></td>
+<td class="dag1"></td>
+<td class="dag2"></td>
+<td class="dag3">Pushdown variant droppsett typ 2</td>
+<td class="dag4">Biceps isolasjon droppsett type 2</td>
 </tr>
 <tr>
-<td class="mandag"></td>
-<td class="tirsdag"></td>
-<td class="onsdag"></td>
-<td class="torsdag"></td>
-<td class="fredag"></td>
-<td class="lørdag"></td>
-<td class="søndag"></td>
-</tr>
-<tr>
-<td class="mandag"></td>
-<td class="tirsdag"></td>
-<td class="onsdag"></td>
-<td class="torsdag"></td>
-<td class="fredag"></td>
-<td class="lørdag"></td>
-<td class="søndag"></td>
+<td class="dag1"></td>
+<td class="dag2"></td>
+<td class="dag3"></td>
+<td class="dag4">Beinpress widowaker</td>
 </tr>
 
 </article>
