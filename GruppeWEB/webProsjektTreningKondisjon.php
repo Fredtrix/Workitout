2049c764f7a2cf549962f29c09a42a39820f687d
matslo <mats_loekken@hotmail.com>
2013-11-12 16:56:14 +0100

    endret

--- GruppeWEB/webProsjektTreningKondisjon.php
+++ GruppeWEB/webProsjektTreningKondisjon.php
@@ -39,4 +39,16 @@ Rask gange; hvert 5. minutt jogger du 1 minutt.
 <p>30 min. rask gange 
 Rask gange; hvert 5. minutt jogger du 1 minutt. 
 30 min til sammen</p><br>
+<h3>4x4 intervall</h3>
+<ol>
+<li>Oppvarmingen bør vare mellom 5 og 10 minutter – og ligge opp mot 70 % av makspuls</li>
+<li>Dragene på intervallene kan vare i 3 til 8 minutter</li>
+<li>Pausene kan være på 3 til 4 minutter, og ligge på ca 70 % av makspuls</li>
+<li>Totalt bør dragene være på mellom 15 og 25 minutter. 5 intervaller på 5min gir 25 min totalt</li>
+<li>Intensiteten på intervallene bør være på 85-95 % av makspuls</li>
+<li>De første minuttene på dragene vil du ligge på mellom 85-90 %, mens du vil ligge på 90-95 % på de 2 siste (hvis du gjennomfører 4×4)</li>
+<li>Løper du intervallene i motbakke (f.eks 5% stigning på tredemølla) vil dette være snillere på beina, samtidig som det blir enklere å få opp intensiteten.</li>
+<li>Med 2-3 intervalløkter i uken kan du regne med en fremgang på 
+0,5 % pr økt. En økning på kun 3 % vil gi 10slag mindre i minuttet – og tilsvare en reduksjon i biologisk alder på hele 10 år!</li>
+</ol>
 </article> 
