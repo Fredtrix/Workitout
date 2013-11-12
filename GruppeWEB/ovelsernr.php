6de722fac87286f3127408b1e6b15537de611eb5
matslo <mats_loekken@hotmail.com>
2013-11-12 12:34:07 +0100

    moro

---
+++ GruppeWEB/ovelsernr.php
@@ -0,0 +1,50 @@
+<script>
+$(document).ready(function linker(){
+  $(".hide").hide(1);
+  $("#trening").click(function(){
+    $(".hide").toggle(200);
+  });
+});
+</script>
+<aside>
+<ul id="navigasjonsbarTrening">
+<li><a href="webProsjektHoved.php?page=1">Om trening</a></li>
+<li id="trening"><a href="#">Treningsprogram</a></li>
+<li class="hide"><a href="webProsjektHoved.php?page=20">Styrke</a></li>
+<li class="hide"><a href="webProsjektHoved.php?page=21">Kondisjon</a></li>
+<li class="hide"><a href="webProsjektHoved.php?page=22">For utøvere</a></li>
+<li><a href="webProsjektHoved.php?page=6">Finn ditt treningssenter</a></li>
+<li><a href="webProsjektHoved.php?page=23">Øvelser</a></li>
+</ul>
+</aside>
+<article>
+<p>Side</p> <a href="webProsjektHoved.php?page=23">1 </a><a href="webProsjektHoved.php?page=24">2 </a>
+<a href="webProsjektHoved.php?page=25">3 </a> <a href="webProsjektHoved.php?page=26">4 </a>
+<a href="webProsjektHoved.php?page=27">5 </a>
+<p>Kabelroing</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/GZbfZ033f74" frameborder="0" allowfullscreen></iframe>
+<p>Kabel torso rotation</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/pAplQXk3dkU" frameborder="0" allowfullscreen></iframe>
+<p>Knebøy</p>
+<iframe width="420" height="315" src="//www.youtube.com/embed/gBnyAeXJnAg" frameborder="0" allowfullscreen></iframe>
+<p>Knestående skrå sit-up</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/NJQROeaBiVE" frameborder="0" allowfullscreen></iframe>
+<p>Leg curl</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/4QX1xgnBEJc" frameborder="0" allowfullscreen></iframe>
+<p>Leg extension</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/YyvSfVjQeL0" frameborder="0" allowfullscreen></iframe>
+<p>Markløft</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/RyJbvWAh6ec" frameborder="0" allowfullscreen></iframe>
+<p>Millitærpress</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/2yjwXTZQDDI" frameborder="0" allowfullscreen></iframe>
+<p>Nedtrekk</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/Lli1RGxW9Ps" frameborder="0" allowfullscreen></iframe>
+<p>Nedtrkk triceps/pushdown</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/2-LAMcpzODU" frameborder="0" allowfullscreen></iframe>
+<p>Omvendt flies</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/g1O6AbIrO3E" frameborder="0" allowfullscreen></iframe>
+<p>Side</p> <a href="webProsjektHoved.php?page=23">1 </a><a href="webProsjektHoved.php?page=24">2 </a>
+<a href="webProsjektHoved.php?page=25">3 </a> <a href="webProsjektHoved.php?page=26">4 </a>
+<a href="webProsjektHoved.php?page=27">5 </a>
+
+</article>
