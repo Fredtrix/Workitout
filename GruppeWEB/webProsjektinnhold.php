1b93b5cd95b446db679363bc2ee014e66bce9fd2
matslo <mats_loekken@hotmail.com>
2013-11-19 14:24:12 +0100

    nå er ting som det skal være

--- GruppeWEB/webProsjektinnhold.php
+++ GruppeWEB/webProsjektinnhold.php
@@ -1,13 +1,5 @@
 <aside>
 	
-<ul id="navigasjonsbarTrening">
-<li><a href="#">Treningsprogram</a></li>
-<li><a href="webProsjektHoved.php?page=6">finn ditt treningssenter</a></li>
-<li><a href="werbProsjektHoved.php?page=2">Kosthold</a></li>
-<li><a href="#">Butikk</a></li>
-<li><a href="#">Forum</a></li>
-<li><a href="#">Kontakt</a></li>
-</ul>
 
 </aside>
 
