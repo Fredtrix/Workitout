e1cd9de313f6e861298de7923a8f4220220b9b2f
Atleaspelien <atleaspelien@hotmail.com>
2013-11-19 12:17:22 +0100

    x

--- GruppeWEB/includekosthold.php
+++ GruppeWEB/includekosthold.php
@@ -33,18 +33,18 @@ $(".hide3").hide(200);
 <aside>
 	<ul id="navigasjonsbarTrening">
 	
-		<li><a href="webProsjektHoved.php?page=2">Generelt om kosthold</a></li>
+		<li><a href="index.php?page=2">Generelt om kosthold</a></li>
 		<li id="Kost4"><a href="#">Kostholds tips</a></li>
-		<li class="hide4"><a href="webProsjektHoved.php?page=9">Tips for å gå ned i vekt </a></li>
-		<li class="hide4"><a href="webProsjektHoved.php?page=10">Tips for å gå opp i vekt </a></li>
-		<li class="hide4"><a href="webProsjektHoved.php?page=11">Dietter du IKKE skal velge </a></li>
+		<li class="hide4"><a href="index.php?page=9">Tips for å gå ned i vekt </a></li>
+		<li class="hide4"><a href="index.php?page=10">Tips for å gå opp i vekt </a></li>
+		<li class="hide4"><a href="index.php?page=11">Dietter du IKKE skal velge </a></li>
 		<li id="Kost2"><a href="#">Kostholds tilskudd</a></li>
-		<li class="hide2"><a href="webProsjektHoved.php?page=21"> BCAA</a></li>
-		<li class="hide2"><a href="webProsjektHoved.php?page=22">Kreatin</a></li>
-		<li class="hide2"><a href="webProsjektHoved.php?page=23">Protein</a></li>
+		<li class="hide2"><a href="index.php?page=21"> BCAA</a></li>
+		<li class="hide2"><a href="index.php?page=22">Kreatin</a></li>
+		<li class="hide2"><a href="index.php?page=23">Protein</a></li>
 		<li id="Kost3"><a href="#">Sunne oppskrifter</a></li>
-		<li class="hide3"><a href="webProsjektHoved.php?page=24">Sunne pannekaker</a></li>
-		<li class="hide3"><a href="webProsjektHoved.php?page=25">Protein barer</a></li>
+		<li class="hide3"><a href="index.php?page=24">Sunne pannekaker</a></li>
+		<li class="hide3"><a href="index.php?page=25">Protein barer</a></li>
 
 	</ul>
 </aside>
